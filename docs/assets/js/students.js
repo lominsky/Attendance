da20b510fc736157f3647900139b8722909f89cf
Louis Minsky <louis@louisminsky.com>
2020-10-26 19:12:23 -0400

    added absent/late tally to the students page

--- docs/assets/js/students.js
+++ docs/assets/js/students.js
@@ -3,7 +3,14 @@ function prepPage() {
 	firebase.database().ref("/students/").once('value', snapshot => {
 		data = snapshot.val();
 		data = data.filter(el => el != null);
+		for(let d of data) {
+			d.Present = 0;
+			d.Late = 0;
+			d.Absent = 0;
+			d["Not Taken"] = 0;
+		}
 		sortData(data, fillTable);
+		getAttendance();
 	}, error => {
 		console.log(error);
 	});
@@ -21,12 +28,45 @@ function fillTable(array) {
 	let table = $("tbody");
 	table.html("");
 	for(let row of array) {
-		let tr = $("<tr onclick='document.location=\"student.html?id=" + row.id +"\"'></tr>");
+		let tr = $("<tr student-id='" + row.id + "' onclick='document.location=\"student.html?id=" + row.id +"\"'></tr>");
 		tr.append($("<td></td>").text(row.id));
 		tr.append($("<td></td>").text(row.first_name));
 		tr.append($("<td></td>").text(row.last_name));
+		tr.append($("<td status='Absent'></td>").text(row.Absent));
+		tr.append($("<td status='Late'></td>").text(row.Late));
 		table.append(tr);
 	}
+}
+
+function getAttendance() {
+	firebase.database().ref("attendance/byStudent").once('value', snapshot => {
+		let attendance = snapshot.val();
+		for(let sid in attendance) {
+			if(attendance[sid] == null) continue;
+			let classAttendance = attendance[sid];
+			let att = {
+				Present: 0,
+				Late: 0,
+				Absent: 0,
+				"Not Taken": 0
+			}
+			for(let c in classAttendance) {
+				if(classAttendance[c] == null) continue;
+				for(let d in classAttendance[c]) {
+					if(classAttendance[c][d] == null) continue;
+					att[classAttendance[c][d].status]++;
+				}
+			}
+			let el = data.find(element => element.id == sid);
+			el.Present = att.Present;
+			el.Late = att.Late;
+			el.Absent = att.Absent;
+			el["Not Taken"] = att["Not Taken"];
+			let row = $('[student-id="' + sid + '"');
+			$(row.find($('[status="Absent"'))).text(att.Absent);
+			$(row.find($('[status="Late"'))).text(att.Late);
+		}
+	})
 }
 
 function addEntry() {
