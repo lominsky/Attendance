1b0c0430f28bae231b748edfa567dfbb6ecc5b1e
Louis Minsky <louis@louisminsky.com>
2020-12-05 15:23:43 -0500

    comment out onBackgroundMessage

--- docs/assets/js/fb.js
+++ docs/assets/js/fb.js
@@ -369,15 +369,15 @@ messaging.onMessage((payload) => {
   // ...
 });
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: 'assets/img/c&c-logo.png'
-  };
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: 'assets/img/c&c-logo.png'
+//   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
